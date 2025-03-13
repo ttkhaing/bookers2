4ad5ede8ac16fbb4fffb6c9c3b75713e07d6068f
ttkhaing <ec2-user@ip-172-31-3-19.ap-northeast-1.compute.internal>
2025-03-13 20:16:04 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+
+  
+
+  resources :users, only: [:show, :edit, :update]
+end
